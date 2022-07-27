8a9adf9206b2497d4c3645b848bc8430ec302b11
Kaushal Mandal <kaushal.mandal@ibm.com>
2022-07-27 17:32:52 +0530

    First commit.

---
+++ src/main/java/com/example/VesselSchedulerEventProcessor/VesselSchedulerEventProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.VesselSchedulerEventProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselSchedulerEventProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesselSchedulerEventProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VesselSchedulerEventProcessor/VesselSchedulerEventProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VesselSchedulerEventProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselSchedulerEventProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
